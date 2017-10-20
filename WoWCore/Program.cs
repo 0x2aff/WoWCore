7b33226df257d70c7b139e1fe67a92284efe2244
0x2aff <schlosser.st@gmail.com>
2017-10-20 21:37:01 +0200

    Add project files.

---
+++ WoWCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WoWCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
