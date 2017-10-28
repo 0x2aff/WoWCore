77a87adf3e64ce15d2cc095b31e96bb7c4b5a50b
0x2aff <schlosser.st@gmail.com>
2017-10-28 23:05:58 +0200

    Add AuthLogonChallengeServer.

---
+++ AuthServer/Packets/AuthLogonChallengeServer.cs
@@ -0,0 +1,35 @@
+﻿/*
+ *  WoWCore - World of Warcraft 1.12 Server
+ *  Copyright (C) 2017 exceptionptr
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using WoWCore.AuthServer.Opcodes;
+
+namespace WoWCore.AuthServer.Packets
+{
+    public class AuthLogonChallengeServer
+    {
+        public AuthCmd Command;
+        public AuthResult Error;
+        public byte[] SrpB;
+        public byte SrpGLength;
+        public byte SrpG;
+        public byte SrpNLength;
+        public byte SrpN;
+        public byte[] SrpSalt;
+
+    }
+}
