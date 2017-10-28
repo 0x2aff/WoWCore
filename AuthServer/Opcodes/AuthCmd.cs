1a653a2e1576e6e785b6dcb195326125941006ff
0x2aff <schlosser.st@gmail.com>
2017-10-28 20:38:11 +0200

    Add AuthCmd.

---
+++ AuthServer/Opcodes/AuthCmd.cs
@@ -0,0 +1,11 @@
+﻿namespace WoWCore.AuthServer.Opcodes
+{
+    public enum AuthCmd : byte
+    {
+        LogonChallenge = 0x00,
+        LogonProof = 0x01,
+        ReconnectChallenge = 0x02,
+        ReconnectProof = 0x03,
+        Realmlist = 0x10
+    }
+}
